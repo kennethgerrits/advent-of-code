965362c8e88ea8f2f936a475d7335fc31a147174
Kenneth Gerrits <kennethgerrits@gmail.com>
2022-12-12 16:48:19 +0100

    Star 2

--- 2022/day-4/Day4.php
+++ 2022/day-4/Day4.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-require_once 'Elf.php';
-
 final class Day4
 {
     public function __construct()
@@ -13,55 +11,28 @@ final class Day4
 
     private function calculate(): int
     {
-        $handle = fopen("input.txt", "r");
+        $handle = fopen("input.txt", "rb");
         $count = 0;
 
         if ($handle) {
             while (($line = fgets($handle)) !== false) {
                 $line = trim($line);
-
                 $elvesCoverage = explode(',', $line);
+
                 $elf1 = $elvesCoverage[0];
                 $elf1Range = explode('-', $elf1);
                 $elf1From = (int) $elf1Range[0];
                 $elf1Till = (int) $elf1Range[1];
-                $elf1NetSpace = $elf1Till - $elf1From;
 
                 $elf2 = $elvesCoverage[1];
                 $elf2Range = explode('-', $elf2);
                 $elf2From = (int) $elf2Range[0];
                 $elf2Till = (int) $elf2Range[1];
-                $elf2NetSpace = $elf2Till - $elf2From;
 
-                if ($elf1NetSpace === $elf2NetSpace && $elf1From === $elf2From && $elf1Till === $elf2Till) {
-                    $count++;
-                    continue;
-                }
-
-                if ($elf1NetSpace > $elf2NetSpace) {
-                    $bigElf = new Elf(
-                        $elf1From,
-                        $elf1Till,
-                        $elf1NetSpace,
-                    );
-                    $smallElf = new Elf(
-                        $elf2From,
-                        $elf2Till,
-                        $elf2NetSpace,
-                    );
-                } else {
-                    $bigElf = new Elf(
-                        $elf2From,
-                        $elf2Till,
-                        $elf2NetSpace,
-                    );
-                    $smallElf = new Elf(
-                        $elf1From,
-                        $elf1Till,
-                        $elf1NetSpace,
-                    );
-                }
-                if ($smallElf->from >= $bigElf->from && $smallElf->till <= $bigElf->till) {
+                if (($elf1From <= $elf2From && $elf1Till >= $elf2Till)
+                    || ($elf1From >= $elf2From && $elf1Till <= $elf2Till)
+                    || ($elf1From <= $elf2Till && $elf2From <= $elf1Till)
+                    ) {
                     $count++;
                 }
             }
